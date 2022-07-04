84caacb71dc2e8480f6dbbf096ac97760674d3d5
Edward Do <edwardtdo@gmail.com>
2022-07-04 18:47:44 +1000

    fully working tic tac toe with win tracker and draw detector

--- js/script.js
+++ js/script.js
@@ -1,89 +1,134 @@
 console.log('is this thing on?');
 let turnCount = 0;
+let xWinCount = 0;
+let oWinCount = 0;
 
-const render = function () {
-    if ($('#board').find('.x1.x').length === 3) {
-        xWins();
-
-    }
-    if ($('#board').find('.x2.x').length === 3) {
-        xWins();
-
-    }
-    if ($('#board').find('.x3.x').length === 3) {
-        xWins();
-
-    }
-    if ($('#board').find('.y1.x').length === 3) {
-        xWins();
-    }
-    if ($('#board').find('.y2.x').length === 3) {
-        xWins();
-    }
-    if ($('#board').find('.y3.x').length === 3) {
-        xWins();
-    }
-    if ($('.square.x1.y3').hasClass('x') && $('.square.x2.y2').hasClass('x') && $('.square.x3.y1').hasClass('x')) {
-        xWins();
-    }
-    if ($('.square.x3.y3').hasClass('x') && $('.square.x2.y2').hasClass('x') && $('.square.x1.y1').hasClass('x')) {
-        xWins();
+const render = function () {   
+    if (turnCount % 2 === 0) {
+        $('#players-turn').text(`X`);
+    } else {
+        $('#players-turn').text('O');
     }
 
-    if ($('#board').find('.x1.o').length === 3) {
-        oWins();
-    }
-    if ($('#board').find('.x2.o').length === 3) {
-        oWins();
-    }
-    if ($('#board').find('.x3.o').length === 3) {
-        oWins();
-    }
-    if ($('#board').find('.y1.o').length === 3) {
-        oWins();
-    }
-    if ($('#board').find('.y2.o').length === 3) {
-        oWins();
-    }
-    if ($('#board').find('.y3.o').length === 3) {
-        oWins();
-    }
-    if ($('.square.x1.y3').hasClass('o') && $('.square.x2.y2').hasClass('o') && $('.square.x3.y1').hasClass('o')) {
-        oWins();
-    }
-    if ($('.square.x3.y3').hasClass('o') && $('.square.x2.y2').hasClass('o') && $('.square.x1.y1').hasClass('o')) {
-        oWins();
-    }
-}
+    $('#new-game').addClass('hidden');
 
-const xWins = function () {
-    $newP = $('<p>');
-    $newP.text('x wins!');
-    $('.container').append($newP);
-}
+    if (turnCount >= 9 && $('body').find('.hidden').length !== 0) {
+        $('#result').text('Draw').removeClass('hidden');
+        $('#new-game').removeClass('hidden');
+        turnCount = 0;
+    }
+    
+    const xWins = function () {
+        $('#result').text('X wins!').removeClass('hidden');
+        xWinCount += 1;
+        $('#x-wins').text(`Wins: ${xWinCount}`);
+        $('#new-game').removeClass('hidden');
+        $('*').removeClass('clickable');
+        turnCount = 0;
+    }
+    
+    const oWins = function () {
+        $('#result').text('O wins!').removeClass('hidden');
+        oWinCount += 1;
+        $('#o-wins').text(`Wins: ${oWinCount}`);
+        $('#new-game').removeClass('hidden');
+        $('*').removeClass('clickable');
+        turnCount = 0;
+    }
 
-const oWins = function () {
-    $newP = $('<p>');
-    $newP.text('o wins!');
-    $('.container').append($newP);
-}
+    $('#x-wins').text(`Wins: ${xWinCount}`);
+    $('#o-wins').text(`Wins: ${oWinCount}`);
+    
+    const winCheck = function () {
+        if ($('#board').find('.x1.x').length === 3) {
+            xWins();
+        }
+        if ($('#board').find('.x2.x').length === 3) {
+            xWins();
+        }
+        if ($('#board').find('.x3.x').length === 3) {
+            xWins();
+        }
+        if ($('#board').find('.y1.x').length === 3) {
+            xWins();
+        }
+        if ($('#board').find('.y2.x').length === 3) {
+            xWins();
+        }
+        if ($('#board').find('.y3.x').length === 3) {
+            xWins();
+        }
+        if ($('.x1.y3').hasClass('x') && $('.x2.y2').hasClass('x') && $('.x3.y1').hasClass('x')) {
+            xWins();
+        }
+        if ($('.x3.y3').hasClass('x') && $('.x2.y2').hasClass('x') && $('.x1.y1').hasClass('x')) {
+            xWins();
+        }
+
+        if ($('#board').find('.x1.o').length === 3) {
+            oWins();
+        }
+        if ($('#board').find('.x2.o').length === 3) {
+            oWins();
+        }
+        if ($('#board').find('.x3.o').length === 3) {
+            oWins();
+        }
+        if ($('#board').find('.y1.o').length === 3) {
+            oWins();
+        }
+        if ($('#board').find('.y2.o').length === 3) {
+            oWins();
+        }
+        if ($('#board').find('.y3.o').length === 3) {
+            oWins();
+        }
+        if ($('.x1.y3').hasClass('o') && $('.x2.y2').hasClass('o') && $('.x3.y1').hasClass('o')) {
+            oWins();
+        }
+        if ($('.x3.y3').hasClass('o') && $('.x2.y2').hasClass('o') && $('.x1.y1').hasClass('o')) {
+            oWins();
+        }
+    }
+    winCheck();
+};
 
 $(document).ready(function() {
-    $('.square').click(function() {
-        turnCount += 1;
-        if (turnCount % 2 === 0 && $(this).hasClass('o') === false) {
-            $(this).find('img').attr('src', './images/x.webp');
+    render();
+
+    $('.clickable').click(function() {
+        if (turnCount % 2 === 0 && $(this).hasClass('clickable') === true) {
+            turnCount += 1;
             $(this).addClass('x');
+            $(this).removeClass('clickable')
             render();
-        } else if (turnCount % 2 !== 0 && $(this).hasClass('x') === false) {
-            $(this).find('img').attr('src', './images/circle-png.webp');
+        } else if (turnCount % 2 !== 0 && $(this).hasClass('clickable') === true) {
+            turnCount += 1;
             $(this).addClass('o');
+            $(this).removeClass('clickable');
             render();
         }
     });
 
+    $('#new-game').on('click', function() {
+        $('.square').removeClass('x o').addClass('clickable');
+        $(this).addClass('hidden');
+        $('#result').addClass('hidden');
+    });
     
 
+    // $('.square').hover (function(){
+    //     $(this).addClass('xhover');
+    //     },
+    //     function() {
+    //     $(this).removeClass('xhover');
+    // })
 
+    // $('.square').hover (function(){
+    //     $(this).addClass('ohover');
+    //     },
+    //     function() {
+    //     $(this).removeClass('ohover');
+    // })
 
 });
