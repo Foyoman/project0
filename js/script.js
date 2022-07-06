bcc611e61ebad72dc6d1e23f7fc458e7d8214bd5
edward <edwardtdo@gmail.com>
2022-07-06 19:53:51 +1000

    removed test files, added readme, improved logic js and dom handling js, added new avatar and yeah

--- js/script.js
+++ js/script.js
@@ -1,111 +1,7 @@
-console.log('is this thing on?');
-let turnCount = 0;
-let xWinCount = 0;
-let oWinCount = 0;
-
-const render = function () {   
-    if (turnCount % 2 === 0) {
-        $('#players-turn').text(`X`);
-    } else {
-        $('#players-turn').text('O');
-    }
-
-    $('#new-game').addClass('hidden');
-
-    if (turnCount >= 9 && $('body').find('.hidden').length !== 0) {
-        $('#result').text('Draw').removeClass('hidden');
-        $('#new-game').removeClass('hidden');
-        turnCount = 0;
-    }
-    
-    const xWins = function () {
-        $('#result').text('X wins!').removeClass('hidden');
-        xWinCount += 1;
-        $('#x-wins').text(`Wins: ${xWinCount}`);
-        $('#new-game').removeClass('hidden');
-        $('*').removeClass('clickable');
-        turnCount = 0;
-    }
-    
-    const oWins = function () {
-        $('#result').text('O wins!').removeClass('hidden');
-        oWinCount += 1;
-        $('#o-wins').text(`Wins: ${oWinCount}`);
-        $('#new-game').removeClass('hidden');
-        $('*').removeClass('clickable');
-        turnCount = 0;
-    }
-
-    $('#x-wins').text(`Wins: ${xWinCount}`);
-    $('#o-wins').text(`Wins: ${oWinCount}`);
-    
-    if (turnCount % 2 === 0) {
-        $('.square').removeClass('O');
-        $('.square').addClass('X', 500);
-    }
-
-    if (turnCount % 2 !== 0) {
-        $('.square').removeClass('X');
-        $('.square').addClass('O', 500);
-    }
-
-    const winCheck = function () {
-        if ($('#board').find('.x1.x').length === 3) {
-            xWins();
-        }
-        if ($('#board').find('.x2.x').length === 3) {
-            xWins();
-        }
-        if ($('#board').find('.x3.x').length === 3) {
-            xWins();
-        }
-        if ($('#board').find('.y1.x').length === 3) {
-            xWins();
-        }
-        if ($('#board').find('.y2.x').length === 3) {
-            xWins();
-        }
-        if ($('#board').find('.y3.x').length === 3) {
-            xWins();
-        }
-        if ($('.x1.y3').hasClass('x') && $('.x2.y2').hasClass('x') && $('.x3.y1').hasClass('x')) {
-            xWins();
-        }
-        if ($('.x3.y3').hasClass('x') && $('.x2.y2').hasClass('x') && $('.x1.y1').hasClass('x')) {
-            xWins();
-        }
-
-        if ($('#board').find('.x1.o').length === 3) {
-            oWins();
-        }
-        if ($('#board').find('.x2.o').length === 3) {
-            oWins();
-        }
-        if ($('#board').find('.x3.o').length === 3) {
-            oWins();
-        }
-        if ($('#board').find('.y1.o').length === 3) {
-            oWins();
-        }
-        if ($('#board').find('.y2.o').length === 3) {
-            oWins();
-        }
-        if ($('#board').find('.y3.o').length === 3) {
-            oWins();
-        }
-        if ($('.x1.y3').hasClass('o') && $('.x2.y2').hasClass('o') && $('.x3.y1').hasClass('o')) {
-            oWins();
-        }
-        if ($('.x3.y3').hasClass('o') && $('.x2.y2').hasClass('o') && $('.x1.y1').hasClass('o')) {
-            oWins();
-        }
-    }
-    winCheck();
-};
-
 const xAvatarImages = {
     'default': 'images/x.webp',
-    'x-wing': 'images/xwing.png'
+    'x-wing': 'images/xwing.png',
+    'shuriken': 'images/Shuriken.png'
 }
 
 const oAvatarImages = {
@@ -115,35 +11,99 @@ const oAvatarImages = {
 };
 
 let xAvatar = 'default';
-
 let oAvatar = 'default';
 
-$('#o-form').on('submit', function(event) {
-    event.preventDefault();
-    oAvatar = $(this).find("[name=o-avatar]").val();
-    $('#oAvatarPic').attr('src', oAvatarImages[oAvatar]);
-});
+const setAvatar = function() {
+    if (turnCount % 2 === 0) {
+        if (xAvatar === 'default') {
+            $('.square').addClass('X');
+        }
+        if (xAvatar === 'shuriken') {
+            $('.square').addClass('shuriken');
+        }
+        if (xAvatar === 'x-wing') {
+            $('.square').addClass('x-wing');
+        }
+        
+    }
 
-$('#x-form').on('submit', function(event) {
-    event.preventDefault();
-    xAvatar = $(this).find("[name=x-avatar]").val();
-    $('#xAvatarPic').attr('src', xAvatarImages[xAvatar]);
-});
+    if (turnCount % 2 !== 0) {
+        if (oAvatar === 'default') {
+            $('.square').addClass('O');
+        }
+        if (oAvatar === 'donut') {
+            $('.square').addClass('donut');
+        }
+        if (oAvatar === 'tie-fighter') {
+            $('.square').addClass('tie-fighter');
+        }
+    }
+}
+
+const resetAvatar = function() {
+    $('.square').removeClass('X');
+    $('.square').removeClass('O');
+    $('.square').removeClass('shuriken');
+    $('.square').removeClass('donut');
+    $('.square').removeClass('x-wing');
+    $('.square').removeClass('tie-fighter');
+}
+
+const render = function () {   
+    winCheck();
+    resetAvatar();
+    setAvatar();
+
+    if (xWin) {
+        $('#result').text('X wins!').css('visibility', 'visible');
+        $('#x-wins').text(`Wins: ${xWinCount}`);
+        $('#new-game').css('visibility', 'visible');
+        $('.square').removeClass('clickable');
+        reset();
+        turnCount += 1;
+    }
+
+    if (oWin) {
+        $('#result').text('O wins!').css('visibility', 'visible');
+        $('#o-wins').text(`Wins: ${xWinCount}`);
+        $('#new-game').css('visibility', 'visible');
+        $('.square').removeClass('clickable');
+        reset();
+    }
+
+    if (arrayX.length + arrayO.length === 9 && xWin !== 1 && oWin !== 1) {
+        $('#result').text('Draw').css('visibility', 'visible');
+        $('#new-game').css('visibility', 'visible');
+        reset();
+    }
+  
+    if (turnCount % 2 === 0) {
+        $('#players-turn').text(`X`);
+    } else {
+        $('#players-turn').text('O');
+    }
+
+    $('#new-game').addClass('hidden');
+
+    $('#x-wins').text(`Wins: ${xWinCount}`);
+    $('#o-wins').text(`Wins: ${oWinCount}`);
+};
 
 $(document).ready(function() {
     render();
-
+    
     $('.clickable').click(function() {
         if (turnCount % 2 === 0 && $(this).hasClass('clickable') === true) {
             turnCount += 1;
-            $(this).addClass('x');
+            const gridValue = $(this).data('value');
+            arrayX.push(gridValue);
             $(this).find('img').attr('src', xAvatarImages[xAvatar]);
             $(this).removeClass('clickable')
             render();
         } else if (turnCount % 2 !== 0 && $(this).hasClass('clickable') === true) {
             turnCount += 1;
-            $(this).addClass('o');
-            // $(this).css('background-image', oAvatarImages[oAvatar]);
+            const gridValue = $(this).data('value');
+            arrayO.push(gridValue);
             $(this).find('img').attr('src', oAvatarImages[oAvatar]);
             $(this).removeClass('clickable');
             render();
@@ -151,53 +111,26 @@ $(document).ready(function() {
     });
 
     $('#new-game').on('click', function() {
-        $('.square').removeClass('x o X O').addClass('clickable');
+        $('.square').removeClass('x o').addClass('clickable');
         $('.square').html('<img>');
-        $(this).addClass('hidden');
-        $('#result').addClass('hidden');
+        $(this).css('visibility', 'hidden');
+        $('#result').css('visibility', 'hidden');
         render();
     });
 
-    // $(".clickable.X").hover(function(){
-    //     $(this).find('img').attr('src', xAvatarImages[xAvatar]);
-    //     }, function(){
-    //     $(this).html('<img>');
-    // });
+    $('#x-form').on('submit', function(event) {
+        event.preventDefault();
+        xAvatar = $(this).find("[name=x-avatar]").val();
+        $('#xAvatarPic').attr('src', xAvatarImages[xAvatar]);
+        resetAvatar();
+        setAvatar();
+    });
     
-    // $(".clickable.O").hover(function(){
-    //     $(this).find('img').attr('src', oAvatarImages[oAvatar]);
-    //     }, function(){
-    //     $(this).html('<img>');
-    // });
-    
-
-
-    //     $('.o').css('background-image' `url:(${oAvatarImages[oAvatar]})`);
-    // });
-
-    // $('.X').hover(function() {
-    //     $(this).addClass('fuck').fadeIn('slow');
-    // })
-
-    // const $imgX = $(`<img>`);
-    // $imgX.attr('src', 'images/x.webp');
-    // $('.X').on('hover', function() {
-    //     $(this).find('img').attr('src', 'images/x.webp').fadeIn(1000);
-    // });
-
-
-    // $('.square').hover (function(){
-    //     $(this).addClass('xhover');
-    //     },
-    //     function() {
-    //     $(this).removeClass('xhover');
-    // })
-
-    // $('.square').hover (function(){
-    //     $(this).addClass('ohover');
-    //     },
-    //     function() {
-    //     $(this).removeClass('ohover');
-    // })
-
+    $('#o-form').on('submit', function(event) {
+        event.preventDefault();
+        oAvatar = $(this).find("[name=o-avatar]").val();
+        $('#oAvatarPic').attr('src', oAvatarImages[oAvatar]);
+        resetAvatar();
+        setAvatar();
+    });
 });
